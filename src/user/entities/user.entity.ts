f947ae2476f5f9cb9baff3bc66be28931fdd5c3d
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-12 16:12:33 +0330

    user res added

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  username: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  phoneNumber: string;
+}
