12c11cfabad1bc08da73be97d1d1c4ede0834832
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-12 16:11:57 +0330

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: 'online-shop',
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
