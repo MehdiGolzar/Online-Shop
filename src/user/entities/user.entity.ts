216ad8c5ffb5cd27648528aaea9f24a8649e1d60
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-16 15:39:07 +0330

    some field added

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,24 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { DataTypeDefaults } from 'typeorm/driver/types/DataTypeDefaults';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ nullable: false })
+  first_name: string;
+
+  @Column({ nullable: false })
+  last_name: string;
 
   @Column({ nullable: false })
   username: string;
@@ -15,5 +30,14 @@ export class User {
   email: string;
 
   @Column({ nullable: false })
-  phoneNumber: string;
+  phone_number: string;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
