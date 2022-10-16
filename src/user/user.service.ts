d937527c3b0870b3074e2d4e381937cc27843d89
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-16 16:10:34 +0330

    create service added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/request/create-user.dto';
+import { UpdateUserDto } from './dto/request/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const isExistsUsername = await this.userRepository.findOne({
+      where: { username: createUserDto.username },
+    });
+
+    if (isExistsUsername) {
+      throw new ConflictException({ message: 'Username already exists' });
+    }
+
+    const isExistsEmail = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (isExistsEmail) {
+      throw new ConflictException({ message: 'Email already exists' });
+    }
+
+    const isExistsPhoneNumber = await this.userRepository.findOne({
+      where: { phone_number: createUserDto.phone_number },
+    });
+
+    if (isExistsPhoneNumber) {
+      throw new ConflictException({ message: 'Phone number already exists' });
+    }
+
+    const newUser = this.userRepository.create(createUserDto);
+
+    return this.userRepository.save(newUser);
   }
 
   findAll() {
