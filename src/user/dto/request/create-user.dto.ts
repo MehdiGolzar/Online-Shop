08dc6830bf9e996fb11dcbcf1941b09ba3caa7ee
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-16 16:09:49 +0330

    updated

---
+++ src/user/dto/request/create-user.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  last_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsPhoneNumber('IR')
+  @IsNotEmpty()
+  phone_number: string;
+
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
+  password: string;
+}
