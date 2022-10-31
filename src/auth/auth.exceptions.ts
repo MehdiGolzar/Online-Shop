cb6a14e08903bb7db51486f0ee12c32f65de44e7
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-10-31 16:09:58 +0330

    updated

--- src/auth/auth.exceptions.ts
+++ src/auth/auth.exceptions.ts
@@ -4,18 +4,16 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ErrorResponse } from 'src/shared/dto/response/response.dto';
-import { SignInErrors } from './enums/auth-messages.enum';
+import { AuthErrors } from './enums/auth-messages.enum';
 
 export class SignUpConflicit extends ConflictException {
-  constructor(message) {
+  constructor(message: string) {
     super(new ErrorResponse(message, HttpStatus.CONFLICT));
   }
 }
 
-export class SignInUnauthorized extends UnauthorizedException {
-  constructor() {
-    super(
-      new ErrorResponse(SignInErrors.UNAUTHORIZED, HttpStatus.UNAUTHORIZED),
-    );
+export class Unauthorized extends UnauthorizedException {
+  constructor(message: string = AuthErrors.UNAUTHORIZED) {
+    super(new ErrorResponse(message, HttpStatus.UNAUTHORIZED));
   }
 }
