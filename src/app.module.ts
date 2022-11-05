ff763f20b482ca178c2699aaad6854964cd4bef8
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-11-05 16:17:37 +0330

    redis added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
   imports: [
@@ -16,6 +18,17 @@ import { AuthModule } from './auth/auth.module';
       password: 'postgres',
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_: ConfigService) => ({
+        config: {
+          host: '127.0.0.1',
+          port: 6379,
+          name: 'redis',
+        },
+      }),
     }),
     UserModule,
     AuthModule,
