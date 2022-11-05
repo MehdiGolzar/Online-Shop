c47660e845994e7496acd69f671ab618bcc82685
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-11-05 16:18:00 +0330

    test kavenegar

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { SignUpDto } from './dto/signUp.dto';
 import { AuthErrors, SignUpErrors } from './enums/auth-messages.enum';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { JwtToken } from './interfaces/jwt-token.interface';
+import * as Kavenegar from 'kavenegar';
 
 @Injectable()
 export class AuthService {
@@ -42,11 +43,30 @@ export class AuthService {
     }
   }
 
+  private async confirmByPhoneNumber(phoneNumber: string) {
+    const apiKey =
+      '6D6A5539734B67676C7365506672586D33665257424E3947526C6A706F43772B466A3650686758465972453D';
+    const api = Kavenegar.KavenegarApi({
+      apikey: apiKey,
+    });
+    api.VerifyLookup(
+      {
+        receptor: '+989129009479',
+        token: '123456',
+        template: 'Verify',
+      },
+      function (response, status) {
+        console.log(response);
+        console.log(status);
+      },
+    );
+  }
+
   async signUp(signUpDto: SignUpDto): Promise<void> {
     const { username, email, phoneNumber } = signUpDto;
 
     await this.isUserDuplicate(username, email, phoneNumber);
-
+    await this.confirmByPhoneNumber(phoneNumber);
     const newUser = this.userRepository.create(signUpDto);
     await this.userRepository.save(newUser);
 
