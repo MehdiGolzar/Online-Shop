1f76061e0a833dc2aef462155ad47a7e0e5d773b
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-11-07 16:27:12 +0330

    readVerifyCode method added to redis proxy

--- src/shared/proxies/redis.proxy.ts
+++ src/shared/proxies/redis.proxy.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { RedisService } from '@liaoliaots/nestjs-redis';
+import { ConfigConstant } from 'src/config/config.const';
 
 @Injectable()
 export class RedisProxy {
   constructor(private redisService: RedisService) {}
 
-  saveVerifyCode() {
+  saveVerifyCode(phoneNumber: string, verifyCode: number) {
     const client = this.redisService.getClient();
+    return client.set(
+      `${phoneNumber}-verify-code:`,
+      verifyCode,
+      'EX',
+      ConfigConstant.smsPanel.otp.expireTime,
+    );
+  }
+
+  readVerifyCode(phoneNumber: string) {
+    const client = this.redisService.getClient();
+    return client.get(`${phoneNumber}-verify-code:`);
   }
 }
