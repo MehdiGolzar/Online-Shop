e508a995ba0f0168ea3fc04bdebd102441618491
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-11-07 16:27:23 +0330

    messages updated

--- src/auth/enums/auth-messages.enum.ts
+++ src/auth/enums/auth-messages.enum.ts
@@ -1,4 +1,4 @@
-export enum SignUpErrors {
+export enum RegisterErrors {
   USERNAEM_CONFLICT = 'Username already exists',
   EMAIL_CONFLICT = 'The email entered has already been registered',
   phoneNumber_CONFLICT = 'The phone number entered has already been registered',
@@ -7,4 +7,5 @@ export enum SignUpErrors {
 export enum AuthErrors {
   LOGIN_FAIL = 'Unauthorized!, Please check your login credentials',
   UNAUTHORIZED = 'Unauthorized!',
+  VERIFICATION_FAILED = 'The verification code is incorrect or has expired'
 }
