95cb0f13f37d3ec24171296a6de3a31f40580f66
MehdiGolzar <mehdigolzar.j@gmail.com>
2022-11-08 15:55:16 +0330

    some methods added

--- src/shared/proxies/redis.proxy.ts
+++ src/shared/proxies/redis.proxy.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import { ConfigConstant } from 'src/config/config.const';
+import { BadRequest } from 'src/auth/auth.exceptions';
+import { RegisterErrors } from 'src/auth/enums/auth-messages.enum';
 
 @Injectable()
 export class RedisProxy {
   constructor(private redisService: RedisService) {}
 
-  saveVerifyCode(phoneNumber: string, verifyCode: number) {
+  async saveVerifyCode(
+    phoneNumber: string,
+    verifyCode: number,
+  ): Promise<string> {
     const client = this.redisService.getClient();
+
+    const isExistVerifyCode = await this.readVerifyCode(phoneNumber);
+
+    if (isExistVerifyCode) {
+      throw new BadRequest(RegisterErrors.VERIFICATION_CODE_SENT);
+    }
+
     return client.set(
       `${phoneNumber}-verify-code:`,
       verifyCode,
@@ -16,8 +28,34 @@ export class RedisProxy {
     );
   }
 
-  readVerifyCode(phoneNumber: string) {
+  async readVerifyCode(phoneNumber: string): Promise<string> {
     const client = this.redisService.getClient();
     return client.get(`${phoneNumber}-verify-code:`);
+  }
+
+  async deleteVerifyCode(phoneNumber: string) {
+    const client = this.redisService.getClient();
+    return client.del(`${phoneNumber}-verify-code:`);
+  }
+
+  async saveTemporaryUser(phoneNumber: string): Promise<string> {
+    const client = this.redisService.getClient();
+
+    return client.set(
+      `temporary-${phoneNumber}-user`,
+      'true',
+      'EX',
+      ConfigConstant.temporaryUserExpireTime,
+    );
+  }
+
+  async readTemporaryUser(phoneNumber: string) {
+    const client = this.redisService.getClient();
+    return client.get(`temporary-${phoneNumber}-user`);
+  }
+
+  async deleteTemporaryUser(phoneNumber: string) {
+    const client = this.redisService.getClient();
+    return client.del(`temporary-${phoneNumber}-user`);
   }
 }
